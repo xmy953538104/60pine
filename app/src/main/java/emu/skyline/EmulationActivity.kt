652bfd5252959bb31a66efeb04436a170c55346c
MY <xmy953538104@gmail.com>
2025-12-18 15:36:47 +0800

    修复重复import Update EmulationActivity.kt

--- app/src/main/java/emu/skyline/EmulationActivity.kt
+++ app/src/main/java/emu/skyline/EmulationActivity.kt
@@ -11,8 +11,6 @@ import android.app.PictureInPictureParams
 import android.app.RemoteAction
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.ActivityInfo
 import android.content.res.AssetManager
 import android.content.res.Configuration
