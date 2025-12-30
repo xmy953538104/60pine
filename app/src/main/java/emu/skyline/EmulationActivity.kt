3836f88d65b12142e60eb79cebeb64b218ea293a
MY <xmy953538104@gmail.com>
2025-12-30 16:40:07 +0800

    调用改为通用调度 Update EmulationActivity.kt

--- app/src/main/java/emu/skyline/EmulationActivity.kt
+++ app/src/main/java/emu/skyline/EmulationActivity.kt
@@ -1020,14 +1020,14 @@ class EmulationActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTo
 
     override fun dispatchKeyEvent(event : KeyEvent) : Boolean {
     return if (inputHandler.handleKeyEvent(event)) {
-        inputHandler.updateControllers()  // <--- 新加这一行
+        inputHandler.update()  // <--- 新加这一行
         true
     } else super.dispatchKeyEvent(event)
 }
 
     override fun dispatchGenericMotionEvent(event : MotionEvent) : Boolean {
     return if (inputHandler.handleMotionEvent(event)) {
-        inputHandler.updateControllers()  // <--- 新加这一行
+        inputHandler.update()  // <--- 新加这一行
         true
     } else super.dispatchGenericMotionEvent(event)
 }
